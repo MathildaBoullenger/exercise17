56829c6ec2e24d2672532765c716f812fc356f56
MathildaBoullenger <mathilda.boullenger@outlook.com>
2023-05-04 18:21:54 +1200

    Back end commit

---
+++ Back-end.js
@@ -0,0 +1,13 @@
+function rollDice() {
+    // get the selected dice type from the user
+    let diceTypeSelect = document.getElementById("dice-type");
+    let diceType = parseInt(diceTypeSelect.value);
+  
+    // generate a random number between 1 and 6
+        let result = Math.floor(Math.random() * diceType) + 1;
+  
+    // display the result in the "result" div
+    var resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "Result: You rolled " + result + " on a " + "D" + diceType +"!";
+  }
+  
